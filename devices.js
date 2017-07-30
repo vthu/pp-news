30e49b1bc346c39ad4c696bd54838654b4900b1c
Vishal Thukral <vthukral@paypal.com>
2017-07-30 23:51:34 +0530

    Devices Text updated

--- devices.js
+++ devices.js
@@ -2,43 +2,43 @@ module.exports = [
     {
         uid: 1,
         titleText: 'Raspberry Pi',
-        mainText: 'A credit-card sized computer that plugs into a computer monitor or TV, and uses a standard keyboard and mouse. A capable little device, it enables people of all ages to explore computing, and to learn how to program in languages like Scratch and Python.',
+        mainText: 'Raspberry Pi: A credit-card sized computer that plugs into a computer monitor or TV, and uses a standard keyboard and mouse. A capable little device, it enables people of all ages to explore computing, and to learn how to program in languages like Scratch and Python.',
         updateDate: new Date().toISOString()
     },
     {
         uid: 2,
         titleText: 'AlphaBot kit',
-        mainText: "An amazing starter kit for robotic development, the AlphaBot robotic platform facilitates activities like line tracking, obstacle avoidance,  speed measuring, IR remote control, video monitoring via network, etc. What's more? Thanks to the rich Raspberry Pi open source resources, and the modular design of AlphaBot, you'll learn how to extend and refit, and finally build your own Raspberry Pi robot.",
+        mainText: "AlphaBot kit: An amazing starter kit for robotic development, the AlphaBot robotic platform facilitates activities like line tracking, obstacle avoidance,  speed measuring, IR remote control, video monitoring via network, etc. What's more? Thanks to the rich Raspberry Pi open source resources, and the modular design of AlphaBot, you'll learn how to extend and refit, and finally build your own Raspberry Pi robot.",
         updateDate: new Date().toISOString()
     },
     {
         uid: 3,
         titleText: 'Leap motion',
-        mainText: "Reach into virtual reality with your bare hands. The Leap Motion Controller captures the movement of your hands and fingers so you can interact with your computer in a whole new way",
+        mainText: "Leap motion: Reach into virtual reality with your bare hands. The Leap Motion Controller captures the movement of your hands and fingers so you can interact with your computer in a whole new way",
         updateDate: new Date().toISOString()
     },
     {
         uid: 4,
         titleText: 'Myself & Amazon Echo',
-        mainText: "Amazon Echo provides hands-free voice control for Amazon Music—just ask for your favorite artist or song, or request a specific genre or lyrics even. Echo is Bluetooth-enabled so you can stream other popular music services like iTunes from your phone. Its dual downward-firing speakers produce 360° omni-directional audio to fill any room with immersive sound. Echo works with devices such as lights, switches, TVs, thermostats, and more devices. And I Alexa—the brain behind Echo enables making calls and messaging. Built on the cloud, the Echo adapts to your speech patterns, vocabulary, and preferences.",
+        mainText: "Myself & Amazon Echo: Amazon Echo provides hands-free voice control for Amazon Music—just ask for your favorite artist or song, or request a specific genre or lyrics even. Echo is Bluetooth-enabled so you can stream other popular music services like iTunes from your phone. Its dual downward-firing speakers produce 360° omni-directional audio to fill any room with immersive sound. Echo works with devices such as lights, switches, TVs, thermostats, and more devices. And I Alexa—the brain behind Echo enables making calls and messaging. Built on the cloud, the Echo adapts to your speech patterns, vocabulary, and preferences.",
         updateDate: new Date().toISOString()
         
     },
     {
         uid: 5,
         titleText: 'PC with GPU computing capability',
-        mainText: 'This new computing model uses massively parallel graphics processors to accelerate applications also parallel in nature.Machine learning algorithms have lots of simple operations that don’t depend on one another. Multiple cores within a GPU can handle simple operations in parallel leading to rapid data processing',
+        mainText: 'PC with GPU computing capability: This new computing model uses massively parallel graphics processors to accelerate applications also parallel in nature.Machine learning algorithms have lots of simple operations that don’t depend on one another. Multiple cores within a GPU can handle simple operations in parallel leading to rapid data processing',
         updateDate: new Date().toISOString()
     },
     {
         uid: 6,
         titleText: '3D printer',
-        mainText: 'Enables creation of a physical object from a three-dimensional digital model, typically by laying down many thin layers of a material in succession',
+        mainText: '3D printer: Enables creation of a physical object from a three-dimensional digital model, typically by laying down many thin layers of a material in succession',
         updateDate: new Date().toISOString()
     },
     {
         uid: 7,
         titleText: 'Proxmark 3 Kit , HackRF One and Ubertooth',
-        mainText: 'The Proxmark targets NFC and RFID frequencies that is capable of both transmitting and receiving while meeting the timing requirements of most proximity protocols. HackRF One, an open source platform which acts like a sound card of computer. The ability to process Digital Signals to Radio waveforms allowing integration of large-scale communication networks is awesome'
+        mainText: 'Proxmark 3 Kit , HackRF One and Ubertooth: The Proxmark targets NFC and RFID frequencies that is capable of both transmitting and receiving while meeting the timing requirements of most proximity protocols. HackRF One, an open source platform which acts like a sound card of computer. The ability to process Digital Signals to Radio waveforms allowing integration of large-scale communication networks is awesome'
     }
 ]
